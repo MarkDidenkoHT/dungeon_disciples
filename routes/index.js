4a3be316f80c8ad67c0a1a5e7f70b2b152ae34d4
Mark <didenko.m@hi-tech.md>
2026-06-18 10:04:05 +0300

    error clean -up

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
+
 const crypto = require('crypto');
 
 const { UNITS } = require('../data/units');
